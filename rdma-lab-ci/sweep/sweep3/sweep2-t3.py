bfc1ac288a3ebaff5e797408772fd8a6af1197ee
pingqiu <pingqiu@gmail.com>
2026-09-14 09:56:04 -0700

    split hw suite t3 helper changes

--- rdma-lab-ci/sweep/sweep3/sweep2-t3.py
+++ rdma-lab-ci/sweep/sweep3/sweep2-t3.py
@@ -5,19 +5,12 @@ BASE=os.environ.get('T3_BASE',os.environ['SWEEP_PRODUCT'])
 PIN=os.environ.get('T3_PRODUCT',BASE)
 HARNESS=os.environ.get('T3_HARNESS',BASE)
 tag=os.environ.get('T3_RESULT_TAG','codex02-sweep3-t3-'+os.environ['T3_TAG'])
-results=Path(os.environ.get('T3_RESULT_ROOT',os.environ['SWEEP_ROOT']))/tag
+results=Path('/data/nvme/testdev')/tag
 share=Path('/mnt/smb/work/share/testops/results')/tag
 source=Path(os.environ['SWEEP_PRODUCT_TREE'])/'enterprise'
 if HARNESS != BASE: source=Path(os.environ['SWEEP_HARNESS_TREE'])/'enterprise'
 scripts=source/'testops/packs/kv/scripts'
-client_host=os.environ['T3_CLIENT_HOST']
-client_user=os.environ['T3_CLIENT_USER']
-client_key=os.environ['T3_CLIENT_KEY']
-client=f'{client_user}@{client_host}'
-server_ip=os.environ['T3_SERVER_IP']
-server_ssh=os.environ['T3_SERVER_SSH']
-rdma_device=os.environ['T3_RDMA_DEVICE']
-gid_index=os.environ['T3_GID_INDEX']
+client='testdev@192.168.1.181'
 client_root='/opt/work/'+tag
 cache=Path(os.environ['SWEEP_STEP05_ROOT'])/'tcp-client'
 if HARNESS != BASE: cache=Path(os.environ['SWEEP_ROOT'])/'t3-client'
@@ -35,24 +28,24 @@ else:
 assert sha(source/'testops/packs/kv/testdata/cache_observation_wire_gate.rs')==provenance['fixture_sha256']
 assert sha(cache/'t3-wire-client')==provenance['binary_sha256']
 server_env=results/'server.env'
-server_env.write_text('export PATH=/usr/local/go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin\n'
+server_env.write_text('export PATH=/usr/local/go/bin:/home/testdev/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin\n'
     +f'TESTOPS_T3_RUN_ROOT={results}\nTESTOPS_ACTIVITY_LOG=/mnt/smb/work/share/testops/WHO-IS-RUNNING\n'
     +'TESTOPS_LOCK_FILE=/mnt/smb/work/share/testops/locks/rdma-lab.lock\n'
-    +f'TESTOPS_T3_SERVER_IP={server_ip}\nTESTOPS_T3_DEVICE={rdma_device}\nTESTOPS_T3_GID_INDEX={gid_index}\nCI_PORT_BASE=27000\n')
+    +'TESTOPS_T3_SERVER_IP=10.0.0.3\nTESTOPS_T3_DEVICE=rocep1s0\nTESTOPS_T3_GID_INDEX=3\nCI_PORT_BASE=27000\n')
 client_env=results/'client.env'
 client_env.write_text('export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin\n'
-    +f'TESTOPS_ACTIVITY_LOG=/mnt/smb/work/share/testops/WHO-IS-RUNNING\nTESTOPS_T3_DEVICE={rdma_device}\nTESTOPS_T3_GID_INDEX={gid_index}\n')
+    +'TESTOPS_ACTIVITY_LOG=/mnt/smb/work/share/testops/WHO-IS-RUNNING\nTESTOPS_T3_DEVICE=rocep1s0\nTESTOPS_T3_GID_INDEX=3\n')
 call(['ssh','-o','BatchMode=yes',client,'mkdir '+client_root])
 call(['scp',str(cache/'t3-wire-client'),str(cache/'provenance.json'),str(scripts/'run_volume_cache_rdma_client_gate.sh'),str(client_env),client+':'+client_root+'/'])
 assert subprocess.check_output(['ssh',client,'sha256sum '+client_root+'/t3-wire-client'],text=True).split()[0]==provenance['binary_sha256']
 cpu={}
-for host in (server_ssh, client):
-    cpu[host]=subprocess.check_output(['ssh',host,'cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor /sys/devices/system/cpu/intel_pstate/no_turbo'],text=True).splitlines()
+for host in ('192.168.1.184','192.168.1.181'):
+    cpu[host]=subprocess.check_output(['ssh','testdev@'+host,'cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor /sys/devices/system/cpu/intel_pstate/no_turbo'],text=True).splitlines()
 runner=str(Path(os.environ['SWEEP_ROOT'])/'bin/sw-test-runner')
 weed=str(Path(os.environ['SWEEP_ROOT'])/'bin/weed')
-volume=os.environ.get('SWEEP_VOLUME_BIN','/opt/work/bin/weed-volume-'+PIN+'-rdma')
+volume='/opt/work/bin/weed-volume-'+PIN+'-rdma'
 scenario=dict(name='rdma-integration-t3',timeout='8m',topology=dict(nodes=dict(server=dict(is_local=True),
-    client=dict(host=client_host,user=client_user,key=client_key))),
+    client=dict(host='192.168.1.181',user='testdev',key='/home/testdev/.ssh/id_ed25519'))),
     phases=[dict(name='stale_descriptor_fence',actions=[dict(action='kv_t3_rdma_gate_run',server_node='server',client_node='client',
         server_script=str(scripts/'run_volume_cache_rdma_gate.sh'),client_script=client_root+'/run_volume_cache_rdma_client_gate.sh',
         server_env_file=str(server_env),client_env_file=client_root+'/client.env',client_binary=client_root+'/t3-wire-client',
